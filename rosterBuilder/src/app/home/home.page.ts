87ff30339dc6e9a7af15ae433e1d45533ba1dab9
pfette1 <pfette1@live.maryville.edu>
2023-11-09 10:19:39 -0500

    added ability to add and remove players from roster

--- rosterBuilder/src/app/home/home.page.ts
+++ rosterBuilder/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,68 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  title = "Roster Builder";
+
+  players = [
+    {
+      name: "Jackson",
+      contactName: "Patrick",
+      phoneNumber: "5025553570"
+    },
+    {
+      name: "Sammy",
+      contactName: "John",
+      phoneNumber: "5029868959"
+    },
+    {
+      name: "Levi",
+      contactName: "Eric",
+      phoneNumber: "5029868794"
+    }
+  ]
+
+  public alertButtons = [
+    {
+      text: 'Cancel',
+      handler: (data: any) => {
+      }
+    },
+    {
+      text: 'Save',
+      handler: (player: any) => {
+        this.players.push(player);
+      }
+    }
+  ];
+
+  public alertInputs = [
+    {
+      name: 'name',
+      placeholder: 'Name'
+    },
+    {
+      contactName: 'contactName',
+      placeholder: 'Contact Name'
+    },
+    {
+      name: 'phoneNumber',
+      placeholder: 'Phone Number'
+    }
+  ];
+
+  constructor(private toastController: ToastController) {}
+
+  async removePlayer(player: any, index: number) {
+    const toast = await this.toastController.create({
+      message: player.name + ' successfully removed',
+      duration: 1500,
+      position: 'bottom',
+      color: 'success',
+    });
+
+    await toast.present();
+
+    this.players.splice(index, 1);
+  }
 
 }
